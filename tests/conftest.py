b226a27671aee292a149cee5b13c1053e6b4fdcc
Serhii Taran <savemypicture1@gmail.com>
2023-08-05 19:08:33 +0300

    refactoring conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,21 @@ from selenium import webdriver
 
 
 @pytest.fixture
-def driver():
+def common_driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.implicitly_wait(10)
-    driver.get("https://the-internet.herokuapp.com/?ref=hackernoon.com")
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def actions_driver(common_driver):
+    common_driver.implicitly_wait(10)
+    common_driver.get("https://the-internet.herokuapp.com/")
+    yield common_driver
+
+
+@pytest.fixture
+def waits_driver(common_driver):
+    common_driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver")
+    yield common_driver
